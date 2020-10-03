5f247cf508d0f11045c57c0c4905b10e7ea57e37
AlexSyrov <1854105@gmail.com>
2020-10-03 16:32:02 +0300

    Решение 2ой задачи

--- js/leson_7/task_1/task_№_2.js
+++ js/leson_7/task_1/task_№_2.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const ingredientsList = document.querySelector('ul');
+
+const elements = ingredients.map(options => {
+  const ingredientsEl = document.createElement('li');
+  ingredientsEl.textContent = options;
+  return ingredientsEl;
+});
+
+console.log(ingredientsList);
+ingredientsList.append(...elements);
