3c86b85e4f900ace516fbda1a1ebe0ca7e8e7753
AlexSyrov <1854105@gmail.com>
2020-10-04 17:10:44 +0300

    решение 6ой задачи

---
+++ js/leson_7/task_1/task_№_6.js
@@ -0,0 +1,10 @@
+const inputEl = document.getElementById('validation-input');
+inputEl.addEventListener('blur', () => {
+  if (inputEl.value.length === Number(inputEl.dataset.length)) {
+    inputEl.classList.add('valid');
+  } else if (inputEl.value.length === 0) {
+    inputEl.classList.add(' ');
+  } else {
+    inputEl.classList.add('invalid');
+  }
+});
