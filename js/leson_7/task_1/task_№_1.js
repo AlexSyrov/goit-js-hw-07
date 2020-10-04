ce102c7e6fe756929fa53f2910283725ef98000a
AlexSyrov <1854105@gmail.com>
2020-10-04 11:23:25 +0300

    изменил решение 1ой задачи

--- js/leson_7/task_1/task_№_1.js
+++ js/leson_7/task_1/task_№_1.js
@@ -1,13 +1,21 @@
+// const categList = document.querySelectorAll('.item');
+// for (let i = 0; i < categList.length; i += 1) {
+//   console.log(`В списке ${categList.length} категории.`);
+// }
+
+// const categTitle = document.querySelectorAll('h2');
+
+// categTitle.forEach(element => console.log('Категория:', element.textContent));
+
+// const categEl = document.querySelectorAll('.item ul');
+// categEl.forEach(element =>
+//   console.log('Количество элементов:', element.children.length),
+// );
+
 const categList = document.querySelectorAll('.item');
-for (let i = 0; i < categList.length; i += 1) {
-  console.log(`В списке ${categList.length} категории.`);
-}
+console.log(`В списке ${categList.length} категории.`);
 
-const categTitle = document.querySelectorAll('h2');
-
-categTitle.forEach(element => console.log('Категория:', element.textContent));
-
-const categEl = document.querySelectorAll('.item ul');
-categEl.forEach(element =>
-  console.log('Количество элементов:', element.children.length),
+categList.forEach(element =>
+  console.log(`Категория: ${element.firstElementChild.textContent}
+    Количество элементов: ${element.lastElementChild.children.length}`),
 );
